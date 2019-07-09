b305f62a4d6e7cadafb8f54548a6f41aea61c475
ruas46 <guilhermeruas@hotmail.com>
2019-07-09 16:23:08 -0300

    Change alt img and responsivety text

--- src/components/template/Responsivety.jsx
+++ src/components/template/Responsivety.jsx
@@ -9,13 +9,13 @@ import CellphoneImg from '../../assets/img/cellphone-img.png'
 const ComponentToTrack = ({ isVisible }) => {
     return <div className="responsivety-imgs">
         <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={isVisible}>
-            <img src={TabletImg} alt="Tablet Image" />
+            <img src={TabletImg} alt="Tablet" />
         </Animated>
         <Animated animationIn="pulse" animationOut="fadeOut" isVisible={isVisible}>
-            <img src={DesktopImg} alt="Desktop Image" />
+            <img src={DesktopImg} alt="Desktop" />
         </Animated>
         <Animated animationIn="bounceInRight" animationOut="fadeOut" isVisible={isVisible}>
-            <img src={CellphoneImg} alt="Cellphone Image" />
+            <img src={CellphoneImg} alt="Cellphone" />
         </Animated>
     </div>;
 }
@@ -27,7 +27,7 @@ const Responsivety = () => {
                 <ComponentToTrack />
             </TrackVisibility>
             <div className="responsivety-text">
-                <p>/* Experience with responsive development */</p>
+                <p>{'/* Experience with responsive development */'}</p>
             </div>
         </div>
     );
