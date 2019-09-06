b3b1b748f9f6acb53328ba394313fd3f7f29ea9d
ruas46 <guilhermeruas@hotmail.com>
2019-09-06 00:25:20 -0300

    Refactored Header and added HeaderLinks Component

---
+++ src/components/template/HeaderLinks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HeaderLinks.css'
+
+export default () =>
+    <div className="headerLinks">
+        <a href="#root" >Home</a>
+        <a href="#degree" >About me</a>
+        <a href="#contact" >Contact</a>
+    </div>
