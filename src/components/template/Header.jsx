34a3726177e7087f9cf1a95ff50521bb10bfa6a3
ruas46 <guilhermeruas@hotmail.com>
2019-09-05 22:53:57 -0300

    Change Header component to class and add event listener on resize

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './Header.css'
 
-export default props =>
-    <header className="header">
-        <a href="#root" >Home</a>
-        <a href="#degree" >About me</a>
-        <a href="#contact" >Contact</a>
-    </header>
+class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          width: window.innerWidth,
+        };
+    }
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    }
+
+    render(){
+        const { width } = this.state;
+        const isMobile = width <= 575;
+        console.log(isMobile)
+
+        return (
+            <header className="header">
+                <a href="#root" >Home</a>
+                <a href="#degree" >About me</a>
+                <a href="#contact" >Contact</a>
+            </header>
+        )
+    }
+}
+
+export default Header
