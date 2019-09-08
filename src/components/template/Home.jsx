f98393863052561fecf94d2bc4e342c6d1b5135f
ruas46 <guilhermeruas@hotmail.com>
2019-09-08 19:08:17 -0300

    Add Transaction effect and change color in home text

--- src/components/template/Home.jsx
+++ src/components/template/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Home.css'
+import {Animated} from "react-animated-css";
+import TrackVisibility from 'react-on-screen';
+
+const ComponentToTrack = ({ isVisible }) => {
+    return <Animated animationIn="fadeInLeft" animationOut="fadeOutLeft" isVisible={isVisible}>
+                <div className="text">
+                    <div className="name">Guilherme Ruas Luiz</div>
+                    <div className="dev">Full Stack &<br/>Front End</div>
+                </div>
+            </Animated>
+}
 
 export default props =>
         <div className="home">
-            <div className="text">
-                <div className="name">Guilherme Ruas Luiz</div>
-                <div className="dev">Full Stack &<br/>Front End</div>
-            </div>
+            <TrackVisibility>
+                <ComponentToTrack />
+            </TrackVisibility>
         </div>
