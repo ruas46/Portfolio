e172e0f61d5d415f85723061fca8b70a552967b8
ruas46 <guilhermeruas@hotmail.com>
2019-09-08 18:39:36 -0300

    Added Text field in Home

--- src/components/template/Home.jsx
+++ src/components/template/Home.jsx
@@ -2,4 +2,9 @@ import React from 'react'
 import './Home.css'
 
 export default props =>
-    <div className="home-img" />
+        <div className="home">
+            <div className="text">
+                <div className="name">Guilherme Ruas Luiz</div>
+                <div className="dev">Full Stack &<br/>Front End</div>
+            </div>
+        </div>
