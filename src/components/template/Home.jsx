4cf806d6c069aa18c53550940c46cd581d704a64
ruas46 <guilhermeruas@hotmail.com>
2019-09-08 17:03:07 -0300

    Added Parallax effect on Home img

--- src/components/template/Home.jsx
+++ src/components/template/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import './Home.css'
-import HomeImg from '../../assets/img/04.jpg'
 
 export default props =>
-    <div className="home-img">
-        <img src={HomeImg} alt="Home" />
-    </div>
+    <div className="home-img" />
