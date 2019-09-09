e32289793d6efec7b1d6ffbb94206a6e83710426
ruas46 <guilhermeruas@hotmail.com>
2019-09-09 16:23:02 -0300

    Fix Header Mobile first mount appearing

--- src/components/template/HeaderMobile.jsx
+++ src/components/template/HeaderMobile.jsx
@@ -5,11 +5,14 @@ import {Animated} from "react-animated-css";
 export default ({ isVisible, children }) =>
     <div>
         {children}
-        <Animated animationIn="fadeInRight" animationOut="fadeOutRight" isVisible={isVisible}>
-            <div className="header-mobile">
-                <a href="#degree" >About me</a>
-                <a href="#experience" >Experience</a>
-                <a href="#contact" >Contact</a>
-            </div>
-        </Animated>
+        { isVisible
+          ? <Animated animationIn="fadeInRight" animationOut="fadeOutRight" isVisible={isVisible}>
+                <div className="header-mobile">
+                    <a href="#degree" >About me</a>
+                    <a href="#experience" >Experience</a>
+                    <a href="#contact" >Contact</a>
+                </div>
+            </Animated>
+          : null
+        }
     </div>
