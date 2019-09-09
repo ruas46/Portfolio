ac20fe874093a26ca8e4a67424f5aaefe32af3dc
ruas46 <guilhermeruas@hotmail.com>
2019-09-08 22:07:03 -0300

    Added Icon in header and change style

--- src/components/template/HeaderLinks.jsx
+++ src/components/template/HeaderLinks.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './HeaderLinks.css'
+import HeaderIcon from '../../assets/img/icon.png'
 
 export default () =>
     <div className="headerLinks">
-        <a href="#root" >Home</a>
+        <a href="/" ><img src={HeaderIcon} alt="Icon" /></a>
         <a href="#degree" >About me</a>
+        <a href="#experience" >Experience</a>
         <a href="#contact" >Contact</a>
     </div>
