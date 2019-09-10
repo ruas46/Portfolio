3d01d5fd11e164684e4b10f93a13b982d3621a25
ruas46 <guilhermeruas@hotmail.com>
2019-09-10 15:34:43 -0300

    Reordered components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css'
 import Header from './components/template/Header'
-import Footer from './components/template/Footer'
 import Content from './components/Content'
+import Footer from './components/template/Footer'
 
 export default props =>
     <div className="app">
+      <Header />
       <Content />
       <Footer />
-      <Header />
     </div>
