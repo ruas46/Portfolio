084f651825f7139f03b9f387fb3c519ca5e404d7
ruas46 <guilhermeruas@hotmail.com>
2019-09-10 17:16:26 -0300

    Updates

---
+++ precache-manifest.76c61ca5cb7f7002f5c3570a8ae873b0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1c72b66ec7cd4129756a18b37919a8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ce56d133b4af4178cf76",
+    "url": "/Portfolio/static/css/2.44bbb5e9.chunk.css"
+  },
+  {
+    "revision": "1ac97e39e7597bc716fa",
+    "url": "/Portfolio/static/css/main.ed2b2a7a.chunk.css"
+  },
+  {
+    "revision": "ce56d133b4af4178cf76",
+    "url": "/Portfolio/static/js/2.1e2dd53b.chunk.js"
+  },
+  {
+    "revision": "1ac97e39e7597bc716fa",
+    "url": "/Portfolio/static/js/main.9ce3d740.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "d4762b29c6db0e38e7db8202810ff8dd",
+    "url": "/Portfolio/static/media/04.d4762b29.jpg"
+  },
+  {
+    "revision": "26859a9786e6cd2eb81db27de7ccc4be",
+    "url": "/Portfolio/static/media/10.26859a97.png"
+  },
+  {
+    "revision": "38236572b1ac38b710feaf59bf61d3b2",
+    "url": "/Portfolio/static/media/22.38236572.png"
+  },
+  {
+    "revision": "4fed7572ae0b4f73947ffd580570c04c",
+    "url": "/Portfolio/static/media/books.4fed7572.jpg"
+  },
+  {
+    "revision": "199017de018a723e905ced8b0f370d27",
+    "url": "/Portfolio/static/media/cat.199017de.jpg"
+  },
+  {
+    "revision": "16699bb814c7ccad4eb62620b17d85e8",
+    "url": "/Portfolio/static/media/coffee.16699bb8.jpg"
+  },
+  {
+    "revision": "be9d2ed7b84312228e24a9d2ce561bd4",
+    "url": "/Portfolio/static/media/cook.be9d2ed7.jpg"
+  },
+  {
+    "revision": "8aaecf6ec56f0814b43528770a6b670b",
+    "url": "/Portfolio/static/media/desktop-img.8aaecf6e.png"
+  },
+  {
+    "revision": "9e09546fe68e619cb7e2ce8e19eb1610",
+    "url": "/Portfolio/static/media/esports.9e09546f.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5b07b5c7cedea0a1be4d279ed4a9e2c1",
+    "url": "/Portfolio/static/media/games.5b07b5c7.jpg"
+  },
+  {
+    "revision": "efedea815a3783c276fb072cd8d884fc",
+    "url": "/Portfolio/static/media/marvel.efedea81.jpg"
+  },
+  {
+    "revision": "4c9c7371e6687e0f0007eb28bf28a8e1",
+    "url": "/Portfolio/static/media/popcorn.4c9c7371.jpg"
+  },
+  {
+    "revision": "ea1e2c502f5c299a0554c98a0a5d7bec",
+    "url": "/Portfolio/static/media/teach.ea1e2c50.jpg"
+  }
+]);
