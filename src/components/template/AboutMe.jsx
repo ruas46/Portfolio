51df182f56be00b7f5a69837c8f2c04b56070d8a
ruas46 <guilhermeruas@hotmail.com>
2019-09-13 15:21:37 -0300

    Added animations to about section

--- src/components/template/AboutMe.jsx
+++ src/components/template/AboutMe.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import './AboutMe.css'
+import {Animated} from "react-animated-css";
+import TrackVisibility from 'react-on-screen';
 import Langueges from './Languages'
 import Degree from './Degree'
 
+const ComponentToTrack = ({ isVisible }) => {
+    return <div id="about-me">
+                <div className="about-me-title">About Me</div>
+                <Animated animationIn="fadeInLeft" animationInDelay={500} isVisible={isVisible}>
+                    <div className="about-me-text">Hi! I am Guilherme Ruas, a web developer focused on crafting great web experiences. Coding have been my passion since the days I started working with computers, but I found myself as development since 2015. <br/>In 2017 I graduated in Computer Science in ULBRA - Brazil, complementing my knowledge, adding my years of experience in IT field, totals eight years, half of this time as Developer in companies and freelancer.</div>
+                </Animated>
+                <Animated animationIn="fadeInRight" animationInDelay={800} isVisible={isVisible}>
+                    <Degree />
+                </Animated>
+                <Animated animationIn="slideInUp" animationInDelay={1200} isVisible={isVisible}>
+                    <Langueges />
+                </Animated>
+            </div>
+}
+
 export default () => 
-        <div id="about-me">
-            <div className="title">About Me</div>
-            <div className="text">Hi! I am Guilherme Ruas, a web developer focused on crafting great web experiences. Coding have been my passion since the days I started working with computers, but I found myself as development since 2015. <br/>In 2017 I graduated in Computer Science in ULBRA - Brazil, complementing my knowledge, adding my years of experience in IT field, totals eight years, half of this time as Developer in companies and freelancer.</div>
-            <Degree />
-            <Langueges />
-        </div>
+        <TrackVisibility partialVisibility once>
+            <ComponentToTrack />
+        </TrackVisibility>
+            
