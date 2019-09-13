161c06fcc64ae582fc2d8a58a927347c3d11eb6d
ruas46 <guilhermeruas@hotmail.com>
2019-09-13 02:11:42 -0300

    Change responsivety animations

--- src/components/template/Responsivety.jsx
+++ src/components/template/Responsivety.jsx
@@ -7,17 +7,24 @@ import TabletImg from '../../assets/img/tablet-img.png'
 import CellphoneImg from '../../assets/img/cellphone-img.png'
 
 const ComponentToTrack = ({ isVisible }) => {
-    return <div className="responsivety-imgs">
-        <Animated animationIn="bounceInLeft" isVisible={isVisible}>
-            <img src={TabletImg} alt="Tablet" />
+    return <React.Fragment>
+        <div className="responsivety-imgs">
+            <Animated animationIn="zoomInDown" animationInDelay={200} isVisible={isVisible}>
+                <img src={TabletImg} alt="Tablet" />
+            </Animated>
+            <Animated animationIn="zoomInDown" animationInDelay={100} isVisible={isVisible}>
+                <img src={DesktopImg} alt="Desktop" />
+            </Animated>
+            <Animated animationIn="zoomInDown" animationInDelay={200} isVisible={isVisible}>
+                <img src={CellphoneImg} alt="Cellphone" />
+            </Animated>
+        </div>;
+        <Animated animationIn="slideInUp" animationInDelay={700} isVisible={isVisible}>
+            <div className="responsivety-text">
+                <p>{'/* Experience with responsive development */'}</p>
+            </div>
         </Animated>
-        <Animated animationIn="pulse" isVisible={isVisible}>
-            <img src={DesktopImg} alt="Desktop" />
-        </Animated>
-        <Animated animationIn="bounceInRight" isVisible={isVisible}>
-            <img src={CellphoneImg} alt="Cellphone" />
-        </Animated>
-    </div>;
+    </React.Fragment>
 }
 
 const Responsivety = () => {
@@ -26,9 +33,6 @@ const Responsivety = () => {
             <TrackVisibility partialVisibility once>
                 <ComponentToTrack />
             </TrackVisibility>
-            <div className="responsivety-text">
-                <p>{'/* Experience with responsive development */'}</p>
-            </div>
         </div>
     );
 }
