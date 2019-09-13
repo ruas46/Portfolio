891639a6033be23a92ad419e2428d7baba721578
ruas46 <guilhermeruas@hotmail.com>
2019-09-13 17:35:25 -0300

    Change responsivety imgs

--- src/components/template/Responsivety.jsx
+++ src/components/template/Responsivety.jsx
@@ -3,20 +3,20 @@ import {Animated} from "react-animated-css";
 import TrackVisibility from 'react-on-screen';
 import './Responsivety.css'
 import DesktopImg from '../../assets/img/desktop-img.png'
-import TabletImg from '../../assets/img/tablet-img.png'
+import NotebookImg from '../../assets/img/notebook-img.png'
 import CellphoneImg from '../../assets/img/cellphone-img.png'
 
 const ComponentToTrack = ({ isVisible }) => {
     return <React.Fragment>
         <div className="responsivety-imgs">
             <Animated animationIn="zoomInDown" animationInDelay={200} isVisible={isVisible}>
-                <img src={TabletImg} alt="Tablet" />
+                <img src={CellphoneImg} id="cellphone" width="100" alt="Cellphone" />
             </Animated>
             <Animated animationIn="zoomInDown" animationInDelay={100} isVisible={isVisible}>
-                <img src={DesktopImg} alt="Desktop" />
+                <img src={DesktopImg} id="desktop" width="280" alt="Desktop" />
             </Animated>
             <Animated animationIn="zoomInDown" animationInDelay={200} isVisible={isVisible}>
-                <img src={CellphoneImg} alt="Cellphone" />
+                <img src={NotebookImg} id="notebook" width="230" alt="Notebook" />
             </Animated>
         </div>
         <Animated animationIn="slideInUp" animationInDelay={700} isVisible={isVisible}>
